58b25f51e308f2d41684e5bf7195c2bd7bec31de
Jonathan Watkins <jonrw97@gmail.com>
2023-02-28 15:12:54 +0200

    rails task manager CRUD basic bootstrap

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+# tasks controller
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.delete
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
